3cb2673d5d2c7cb60ec8ef45529cdeec92d268ac
RafaelaLucia <rafaluciadev@gmail.com>
2021-12-13 17:01:02 -0300

    api gufi noSql

---
+++ Sprint-2-BackEnd/senai_gufi_webAPI/Domains/Localizacao.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace senai_gufi_webAPI.Domains
+{
+    public class Localizacao
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("latitude")] //mudar o nome no banco
+        [BsonRequired] //definir que o campo é obrigatório
+        public string Latitude { get; set; }
+        [BsonRequired]
+        public string Longitude { get; set; }
+    }
+}
